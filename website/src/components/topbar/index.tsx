5deb3d292b64cf288a053e97af5f16d2185a8757
wuttisakguy <74335903+wuttisakguy@users.noreply.github.com>
2024-03-17 10:56:16 +0700

    update topbar

--- website/src/components/topbar/index.tsx
+++ website/src/components/topbar/index.tsx
@@ -6,17 +6,13 @@ const Topbar: FC = () => {
   return (
     <div className="w-full h-20 bg-white py-4 px-6 shadow-lg shadow-black/10 flex justify-between items-center select-none">
       <div className="flex justify-start items-center gap-2">
-        <div className="w-[20px]">
-          <FontAwesomeIcon icon={faHome} className="text-[#595959]" />
-        </div>
-        <p className="text-[#595959]">/</p>
         <p className="text-[#595959]">dashboard</p>
       </div>
       <div className="flex justify-end items-center gap-4 p-3 rounded-lg duration-300 hover:bg-black/5 cursor-pointer">
           <div className="w-[15px]">
                <FontAwesomeIcon icon={faUser} className="text-[#595959]" />
           </div>
-          <p className="text-[#595959]">Username</p>
+          <p className="text-[#595959]">Admin</p>
       </div>
     </div>
   );
